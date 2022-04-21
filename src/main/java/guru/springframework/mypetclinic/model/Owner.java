1f5e74ba9485e00feeee5ef92136add1e03ec2ad
Santy288 <santycmn28@gmail.com>
2022-04-21 15:23:36 +0200

    Add POJOs

---
+++ src/main/java/guru/springframework/mypetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package guru.springframework.mypetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/guru/springframework/mypetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.mypetclinic.model;
+
+public class Vet extends Person {
+
+}
